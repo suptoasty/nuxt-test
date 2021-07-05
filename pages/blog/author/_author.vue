27009c6a69bc56758ffaa2c8e03634950013496c
suptoasty <jason.lonsinger@gmail.com>
2021-07-04 20:28:44 -0500

    fix author section

--- pages/blog/author/_author.vue
+++ pages/blog/author/_author.vue
@@ -1,20 +1,45 @@
 <template>
-	<div>
-		<h1>Author: {{ articles[0].author.name }}</h1>
-		<p>Bio: {{ articles[0].author.bio }}</p>
-		<h3>Here are a list of articles by {{ articles[0].author.name }}</h3>
-		<ul>
-			<li v-for="article in articles" :key="article.slug">
-				<nuxt-link :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-					<img src="article.image" alt="article.alt" />
-					<div>
-						<h2>{{ article.title }}</h2>
-						<p>{{ article.description }}</p>
-						<p>{{ formatDate(article.updatedAt) }}</p>
-					</div>
-				</nuxt-link>
-			</li>
-		</ul>
+	<div class="unblurText">
+		<navbar-card class="pa-4" />
+		<v-col cols="12">
+			<h1>Author: {{ articles[0].author.name }}</h1>
+			<v-avatar rounded>
+				<v-img :src="articles[0].author.image"></v-img>
+			</v-avatar>
+			<p>Bio: {{ articles[0].author.bio }}</p>
+			<h3>Here are a list of articles by {{ articles[0].author.name }}</h3>
+		</v-col>
+		<v-col>
+			<search-bar />
+		</v-col>
+		<v-row justify="center" align="center" align-content="center">
+			<v-col
+				align-self="center"
+				xl="2"
+				lg="4"
+				md="6"
+				sm="12"
+				xs="12"
+				v-for="article in articles"
+				:key="article.slug"
+			>
+				<v-lazy>
+					<!-- <devto-card
+						v-if="!!article.id"
+						:article="article"
+						:id="article.id"
+						:title="article.title"
+						:description="article.description"
+					/> -->
+					<blog-card
+						:article="article"
+						:route="article.slug"
+						:title="article.title"
+						:description="article.description"
+					/>
+				</v-lazy>
+			</v-col>
+		</v-row>
 	</div>
 </template>
 
@@ -38,9 +63,9 @@ export default {
 		)
 
 		if (devtoArticles.length > 0) {
-			articles = articles.concat(
-				devtoArticles.map((article) => toPost(article))
-			)
+			// articles = articles.concat(
+			// 	devtoArticles.map((article) => toPost(article))
+			// )
 		}
 
 		return { articles }
